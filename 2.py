5937d34f1e4a3da1ffbf7089e2f2fee515ea4551
luoshuqing2001 <xingruyu@sjtu.edu.cn>
2021-03-03 15:25:51 +0800

    wrote another python file

---
+++ 2.py
@@ -0,0 +1,20 @@
+class solution:
+    def prime(self,n):
+        if n < 3:
+            return 0
+        prim = [1]*n  # 下标0~n-1代表数据0~n-1
+        prim[0] = prim[1] = 0
+        for each in range(2,int(n**0.5)+1):
+            if prim[each]:
+                t = 1
+                while each*(t+1) < n:
+                    prim[each*(t+1)] = 0
+                    t += 1
+        for each in range(0,n):
+            if prim[each]:
+                print(each,end=' ')
+        print('\n')
+        return sum(prim)
+
+s = solution()
+result = s.prime(200)
